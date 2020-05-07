1150720e0a6fec038236eda3e8cb4a9ea835bec6
Priyobrato Das <priyodas12@gmail.com>
2020-05-08 01:47:58 +0530

    Initial commit

---
+++ microservice-beta-profile-service/src/main/java/io/priyospace/microservicebetaprofileservice/MicroserviceBetaProfileServiceApplication.java
@@ -0,0 +1,13 @@
+package io.priyospace.microservicebetaprofileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceBetaProfileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceBetaProfileServiceApplication.class, args);
+	}
+
+}
---
+++ microservice-beta-profile-service/src/test/java/io/priyospace/microservicebetaprofileservice/MicroserviceBetaProfileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.priyospace.microservicebetaprofileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceBetaProfileServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
